4f8160d15520673c9bb002f9a4434fc1c97d61fa
Sean Murphy <filmmakersean@fastmail.net>
2019-05-03 17:05:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,5 +13,10 @@ end
 def printer(names)
   badges = batch_badge_creator(names)
   rooms = assign_rooms(names)
-  
+  count = 0
+  names.count.times do
+    puts badges[count]
+    puts rooms[count]
+    count += 1
+  end
 end
